d1a8dc336299b965b9bfe8323ed51861c091ce9a
eropa <eropa@live.ru>
2018-07-04 13:34:49 +0300

    Фамбричный метод

--- fabricapatern.php
+++ fabricapatern.php
@@ -7,28 +7,49 @@
     <body>
         <?php
             echo "Фабричный метод (Порождающий паттерн)";
-
-
-            //
+            // Абстрактный класс для класса ProductA и ProductB
             abstract class Product{}
-            //
-            class ProductA extends Product{}
-            //
-            class ProductB extends Product{}
-            // абстрактный класс фабрики
+            //класс ProductA
+            class ProductA extends Product{
+                //Выводим сообщение
+                public function show(){
+                    echo 'A';
+                }
+            }
+            //класс ProductA
+            class ProductB extends Product{
+                // Выводим сообщение
+                public function show(){
+                    echo 'B';
+                }
+            }
+            // абстрактный класс фабрики, для создания под классов
             abstract class FactoryAbstract{
+                // метод создания
                 public function create($type){
                     switch($type){
+                        //создаем на класс ProductA
                         case 'A':return new ProductA();
+                        //создаем на класс м
                         case 'B':
                         default:
                             return new ProductB();
                     }
                 }
             }
-            //
-            class Factory
+            //Класс фабрика
+            class Factory extends FactoryAbstract{}
 
+            // Экземпляр класс Фабрика
+            $MyClassFactory=new Factory();
+            //Создаем класс ProductA
+            $productA=$MyClassFactory->create('A');
+            //Выводим сообшение
+            $productA->show();
+            //Создаем класс ProductB
+            $productB=$MyClassFactory->create('B');
+            //Выводим сообшение
+            $productB->show();
 
         ?>
 
