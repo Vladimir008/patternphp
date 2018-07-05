d337bddad92309851f5933887bc6c2dcc28063f5
eropa <eropa@live.ru>
2018-07-05 15:07:36 +0300

    Абстрактная фабрика (добавление комментарий)

--- fabricaabstractpattern.php
+++ fabricaabstractpattern.php
@@ -5,51 +5,67 @@
     <body>
         <h1> Абстрактная фабрика</h1>
         <?php
-
+            // Интерфейс Абстрактной фабрики
             interface AbstractFactory{
+                // Процедура для класса AbstracrtProductA
                 public function createProductA():AbstracrtProductA;
+                // Процедура для класса AbstracrtProductB
                 public function createProductB():AbstracrtProductB;
             }
 
+            // Класс Абстрактной фабрики
             class ConreteFactory1 implements AbstractFactory
             {
+                // Создаем класс ConcreteProductA1 который основывается на AbstracrtProductA
                 public function createProductA():AbstracrtProductA{
                     return new ConcreteProductA1();
                 }
+                // Создаем класс ConcreteProductB1 который основывается на AbstracrtProductB
                 public function createProductB():AbstracrtProductB{
                     return new ConcreteProductB1();
                 }
             }
 
+            // Интерфейс
             interface AbstracrtProductA
             {
+                // Объявляем функцию
                 public function usefulFunctionA():string;
             }
 
+            // Класс AbstracrtProductA1
             class AbstracrtProductA1 implements AbstracrtProductA{
+                // Возрощаем значение
                 public function usefulFunctionA(): string
                 {
                     return "The result of the product A1";
                 }
             }
 
+            // Интерфейс
             interface AbstracrtProductB
             {
+                // Объявляем функцию
                 public function usefulFunctionB:string;
             }
-
+            // Класс
             class AbstractProductB1 implements AbstracrtProductB
             {
+                // Возрощаем значение
                 public function usefulFunctionB():string
                 {
                     return "The result of the product B1";
                 }
             }
 
+            // Экземпляр класса абстактной фабрики
             $factory =new ConreteFactory1();
+            //$producta класса AbstracrtProductA1
             $producta= $factory->createProductA();
+            //$producta класса AbstractProductB1
             $productb= $factory->createProductB();
 
+            // выводим сообшение
             print($producta->usefulFunctionA());
             print($productb->usefulFunctionB());
 
